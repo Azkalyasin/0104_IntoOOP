424f9c57ef285216729451a854eedf916582dcc5
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-04-22 15:26:33 +0700

    perbaikan kesalahan karena menuliskan class di dalam int main()

--- 0104_IntoOOP/0104_IntoOOP.cpp
+++ 0104_IntoOOP/0104_IntoOOP.cpp
@@ -2,6 +2,42 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa {
+public:
+	string nama;
+	int umur;
+	string jurusan;
+
+	void output() {
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << "Jurusan: " << jurusan << endl;
+	}
+};
+
+class matakuliah {
+private:
+	string kodeMK;
+	string namaMK;
+	int sks;
+public:
+	void input() {
+		cout << "Kode MK: ";
+		cin >> kodeMK;
+		cout << "Nama MK: ";
+		cin >> namaMK;
+		cout << "SKS: ";
+		cin >> sks;
+	}
+
+	void output() {
+		cout << "Kode MK: " << kodeMK << endl;
+		cout << "Nama MK: " << namaMK << endl;
+		cout << "sks: " << sks << endl;
+	}
+
+};
+
 int main()
 {
 	class mahasiswa {
