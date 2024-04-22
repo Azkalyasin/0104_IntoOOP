a7b51b37970f9efbb1c4f4d9dab98a86c2b5dccc
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-04-22 15:14:20 +0700

    memasukkan object pada calss mahasiswa

--- 0104_IntoOOP/0104_IntoOOP.cpp
+++ 0104_IntoOOP/0104_IntoOOP.cpp
@@ -6,6 +6,14 @@ int main()
 {
 	class mahasiswa {
 	public:
+		string nama;
+		int umur;
+		string jurusan;
 
+		void output() {
+			cout << "Nama: " << nama << endl;
+			cout << "Umur: " << umur << endl;
+			cout << "Jurusan: " << jurusan << endl;
+		}
 	};
 }
